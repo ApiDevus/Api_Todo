9bfe255f4575ee0b2ce231bd61cf7d4c48e166c6
Quentin <quentin.bourgeois@epitech.eu>
2021-04-06 18:27:55 +0200

    [+] Modification for exemple in bootstrap

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const port = 3000;
 
 app.get('/', (req, res) => {
     res.send("Hello World");
-})
+});
+
+app.listen(port, () => {
+    console.log(`Example app listenning at http://localhost:${port}`);
+});
